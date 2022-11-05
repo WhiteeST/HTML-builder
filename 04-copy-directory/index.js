43c728617b4b6d2d3a98a96c2918ae6dec1f2003
WhiteeST <whiteeluck123@gmail.com>
2022-11-05 19:52:16 +0500

    feat: 06 build page

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,21 +1,31 @@
 const path = require('path');
 const fs = require('fs');
-
+const fsPromises = require('fs').promises;
+// const fs = require('fs/promises');
 const copyPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir(copyPath, { recursive: true }, (err) => {
-    // => [Error: EPERM: operation not permitted, mkdir 'C:\']
-});
-
-const file = fs.readdir(path.join(__dirname, 'files'), { encoding: 'utf8', withFileTypes: true }, (err, files) => {
-    if (err) throw err;
-    files.forEach(file => {
-        const filePathSource = path.join(__dirname, 'files', file.name);
-        const filePathDestination = path.join(copyPath, file.name);
-        fs.copyFile(filePathSource, filePathDestination, (err) => {
+async function removeDir() {
+    try {
+        await fsPromises.rm(copyPath, { recursive: true, force: true });
+        fs.mkdir(copyPath, { recursive: true }, (err) => {
             if (err) throw err;
-            //Переписать с Callback => Промисы => Async await;
-            //Сначала проходятся по файлам, а потом после загрузки копируются
+            // => [Error: EPERM: operation not permitted, mkdir 'C:\']
         });
-    });
-});
+
+        const file = fs.readdir(path.join(__dirname, 'files'), { encoding: 'utf8', withFileTypes: true }, (err, files) => {
+            if (err) throw err;
+            files.forEach(file => {
+                const filePathSource = path.join(__dirname, 'files', file.name);
+                const filePathDestination = path.join(copyPath, file.name);
+                fs.copyFile(filePathSource, filePathDestination, (err) => {
+                    if (err) throw err;
+                    //Переписать с Callback => Промисы => Async await;
+                    //Сначала проходятся по файлам, а потом после загрузки копируются
+                });
+            });
+        });
+    } catch (err) {
+        console.error('Error occurred while reading directory!', err);
+    }
+}
+removeDir();
